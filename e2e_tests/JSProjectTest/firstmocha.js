180f70f97567ac7c770a97c5d3663f10d9647cf6
ExecuteAutomation <kartmcad@gmail.com>
2021-10-20 07:43:58 +1300

    First commit

---
+++ e2e_tests/JSProjectTest/firstmocha.js
@@ -0,0 +1,49 @@
+const { Builder } = require('@tpio/javascript-opensdk');
+const { By } = require('selenium-webdriver');
+
+describe('First Mocha Test', async () => {
+
+   let driver
+   beforeEach(() => {
+        driver = new Builder().forBrowser('chrome').withToken('dVQG8TQ0qCG3jKb_KIGa69b6V-xdkT_NFdsV9-VlwqY1').build()
+
+    });
+
+    it('should pass', async () =>{
+        await driver.get("http://eaapp.somee.com/")
+        // var driver = await new Builder().forBrowser('chrome').withToken('dVQG8TQ0qCG3jKb_KIGa69b6V-xdkT_NFdsV9-VlwqY1').build()
+        // await driver.get("http://eaapp.somee.com/")
+        await driver.findElement(By.linkText("Login")).click()
+        // 3 | type | id=UserName | admin
+        await driver.findElement(By.id("UserName")).sendKeys("admin")
+        // 4 | type | id=Password | password
+        await driver.findElement(By.id("Password")).sendKeys("password")
+        // 5 | click | css=.btn | 
+        await driver.findElement(By.css(".btn")).click()
+        // 6 | click | linkText=Employee List | 
+        await driver.findElement(By.linkText("Employee List")).click()
+        // 7 | click | linkText=Create New | 
+        await driver.findElement(By.linkText("Create New")).click()
+        // 8 | type | id=Name | FirstTestUser
+        await driver.findElement(By.id("Name")).sendKeys("FirstTestUser")
+        // 9 | type | id=Salary | 1000
+        await driver.findElement(By.id("Salary")).sendKeys("1000")
+        // 10 | type | id=DurationWorked | 12321
+        await driver.findElement(By.id("DurationWorked")).sendKeys("12321")
+        // 11 | type | id=Grade | 12
+        await driver.findElement(By.id("Grade")).sendKeys("12")
+        // 12 | type | id=Email | firsttestuser@ea.com
+        await driver.findElement(By.id("Email")).sendKeys("firsttestuser@ea.com")
+        // 13 | click | css=.btn | 
+        await driver.findElement(By.css(".btn")).click()
+        // 14 | click | linkText=Log off | 
+        await driver.findElement(By.linkText("Log off")).click()
+
+
+
+        await driver.quit();
+
+        expect(true).toBe(true);
+
+    });
+});
---
+++ node_modules/@tpio/javascript-opensdk/dist/src/sdk/exceptions/invalidArgumentException.d.ts
@@ -0,0 +1,2 @@
+export default class InvalidArgumentException extends Error {
+}
